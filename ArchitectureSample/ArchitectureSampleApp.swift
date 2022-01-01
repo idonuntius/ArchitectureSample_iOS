41654ea603d269d91945bf41cdb8cb350309b5ad
idonuntius <mc.natsu831@gmail.com>
2022-01-01 21:36:38 +0900

    first commit

---
+++ ArchitectureSample/ArchitectureSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureSampleApp.swift
+//  ArchitectureSample
+//
+//  Created by 井戸田捺貴 on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArchitectureSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArchitectureSample
+//
+//  Created by 井戸田捺貴 on 2022/01/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
